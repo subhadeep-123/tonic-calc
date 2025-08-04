a9690762ef1527bd6ba27307998e40ae41c62f29
Your Name <subhadeep762@gmail.com>
2025-08-05 02:00:50 +0530

    Fix issue with client and serer running

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,7 +2,9 @@ use tonic_micro::{client, config};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let settings = config::load()?;
+    let mut settings = config::load()?;
+
+    settings.server.address = format!("http://{}", settings.server.address);
 
     client::run(settings).await?;
 
