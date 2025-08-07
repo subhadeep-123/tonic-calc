81a9f8bdea47305953f7bcdc739ab9248fe1c4aa
Your Name <subhadeep762@gmail.com>
2025-08-07 13:10:31 +0530

    Add Authentication Interpretor

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,7 +1,11 @@
 mod grpc;
+mod interceptor;
 
-use crate::{client::grpc::send_add_request, config::Settings};
+use crate::{client::grpc::GrpcClient, config::Settings};
 
 pub async fn run(settings: Settings) -> Result<(), Box<dyn std::error::Error>> {
-    send_add_request(settings).await
+    let mut client = GrpcClient::connect(&settings).await?;
+    client.add(10, 20).await?;
+    client.subtract(45, 20).await?;
+    Ok(())
 }
